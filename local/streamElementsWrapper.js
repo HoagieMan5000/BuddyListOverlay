3cbc2f80c03ea92a20be002c92d1f001edffba00
Jon LaBroad <jon.labroad@gmail.com>
2023-04-01 19:51:47 -0700

    initial

---
+++ local/streamElementsWrapper.js
@@ -0,0 +1,38 @@
+const channel = "thugshells";
+
+function isMod(username) {
+  return username.length % 3 === 0;
+}
+
+function isVip(username) {
+  if (!isMod(username)) {
+    return username.length % 4 === 0;
+  }
+  return false;
+}
+
+const twitchClient = new TwitchChatClient(
+  "justinfan777",
+  channel,
+  undefined,
+  (msg) => {
+    const opts = {};
+    const badges = [];
+    if (isMod(msg.username)) {
+      opts.mod = "1";
+      badges.push({ type: "moderator" });
+    } else if (isVip(msg.username)) {
+      badges.push({ type: "vip" });
+    }
+
+    generateChatMessage(
+      {
+        username: msg.username,
+        badges: badges,
+      },
+      msg.message,
+      opts
+    );
+  }
+);
+twitchClient.connect();
